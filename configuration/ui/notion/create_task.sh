603cd38974d28951b889732eb249e5cac04797b6
migueel15 <miguelangeldorado10@gmail.com>
2024-01-07 20:45:54 +0100

    fix: creating task in notion when empty name

--- configuration/ui/notion/create_task.sh
+++ configuration/ui/notion/create_task.sh
@@ -2,21 +2,23 @@ source ~/.config/awesome/configuration/ui/notion/config.sh
 
 TITLE=$(rofi -dmenu -p "Enter title:" -config "~/.config/awesome/configuration/ui/notion/create_task.rasi")
 
-curl 'https://api.notion.com/v1/pages' \
-  -H 'Authorization: Bearer '"$NOTION_API_KEY"'' \
-  -H "Content-Type: application/json" \
-  -H "Notion-Version: 2022-06-28" \
-  --data '{
-	"parent": { "database_id":  "'"$NOTION_DATABASE_ID"'"},
-	"properties": {
-		"Nombre de tarea": {
-			"title": [
-				{
-					"text": {
-						"content": "'"$TITLE"'"
-					}
-				}
-			]
-		}
-	}
-}'
+if [[ -n $TITLE ]]; then
+  curl 'https://api.notion.com/v1/pages' \
+    -H 'Authorization: Bearer '"$NOTION_API_KEY"'' \
+    -H "Content-Type: application/json" \
+    -H "Notion-Version: 2022-06-28" \
+    --data '{
+    "parent": { "database_id":  "'"$NOTION_DATABASE_ID"'"},
+    "properties": {
+      "Nombre de tarea": {
+        "title": [
+          {
+            "text": {
+              "content": "'"$TITLE"'"
+            }
+          }
+        ]
+      }
+    }
+  }'
+fi
